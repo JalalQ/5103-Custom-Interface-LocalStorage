d38035bf63b915dc30f8a055631dec2945a47d85
Jalaluddin Qureshi <58306478+JalalQ@users.noreply.github.com>
2021-05-20 22:22:40 -0400

    Add files via upload

---
+++ lab10.js
@@ -0,0 +1,42 @@
+﻿//LAB 10-DATA STORAGE: INDEX PAGE
+//alert("home");//DELETE AFTER CONFIRMING CONNECTION
+
+
+
+window.onload = function(){
+    
+    var button = document.getElementById("buttonBox");
+    var buttondel = document.getElementById("btnDel");
+    
+    function refreshPage(){
+        
+        var userName = document.getElementById("inName").value;
+        localStorage.setItem("name", userName);
+        
+        var backgroundColor = document.getElementById("inColor").value;
+        localStorage.setItem("color", backgroundColor);
+        
+        window.location.reload();
+
+    }
+    
+    function deleteInfo() {
+        localStorage.clear();
+        window.location.reload();
+    }
+    
+    button.onclick = refreshPage;
+    buttondel.onclick = deleteInfo;
+    
+    var uName = localStorage.getItem("name");
+    if (uName) {
+        document.getElementById("newMsgBox").innerHTML = "Welcome " + uName + "!";
+    }
+    
+    var uColor = localStorage.getItem("color");
+    if (uColor) {
+        document.body.style.background = uColor;
+    }
+
+    
+}
---
+++ products.js
@@ -0,0 +1,20 @@
+﻿//LAB 10-DATA STORAGE: PRODUCTS PAGE
+//alert("products");//DELETE AFTER CONFIRMING CONNECTION
+
+
+
+window.onload = function(){
+    
+    var uName = localStorage.getItem("name");
+    
+    if (uName) {
+        document.getElementById("newMsgBox").innerHTML = "Welcome " + uName + "!";
+    }
+    
+    var uColor = localStorage.getItem("color");
+    
+    if (uColor) {
+        document.body.style.background = uColor;
+    }
+
+}
